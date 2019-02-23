8def5e60a2ff1470e050b8244904b0844924b6e0
Steve Gardner <github@ste.vg>
2019-02-22 16:28:30 -0800

    fixes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import './app.scss';
 import HTML from './app.html';
 import { Component } from '../common/component';
-import { forceSimulation, forceLink, forceManyBody, forceCenter, select, forceCollide, range, max } from 'd3';
+import { forceSimulation, forceLink, forceManyBody, forceCenter, select, forceCollide, range, max, Simulation, event, drag } from 'd3';
 
 interface Node
 {
@@ -14,7 +14,8 @@ interface Link
 {
     source: string,
     target: string,
-    value: number
+    value: number,
+    distance?: number
 }
 
 export class App extends Component
@@ -25,7 +26,12 @@ export class App extends Component
     private _width:number = 0;
     private _height:number = 0;
     private padding:number = 10;
-    private hexSize:number = 30;
+    private hexSize:number = 50;
+    private sim:any;
+    private animationSpeed:number = 500;
+    private opacity:number = 0;
+
+    private linkSelector:any;
 
     private data:{nodes: Node[], links: Link[]} = {
         nodes: [
@@ -41,20 +47,20 @@ export class App extends Component
             {id: '10', name: 'Atque provident', group: 3}
         ],
         links: [
-            {source: '1', target: '2', value: 10},
+            {source: '1', target: '2', value: 14},
             {source: '1', target: '3', value: 1},
             {source: '1', target: '4', value: 3},
-            {source: '1', target: '6', value: 11},
+            {source: '1', target: '6', value: 6},
             {source: '2', target: '3', value: 5},
             {source: '2', target: '5', value: 5},
             {source: '3', target: '4', value: 7},
             {source: '3', target: '5', value: 2},
             {source: '3', target: '6', value: 2},
-            {source: '4', target: '5', value: 9},
-            {source: '4', target: '6', value: 1},
-            {source: '5', target: '6', value: 4},
-            {source: '6', target: '7', value: 4},
-            {source: '6', target: '8', value: 10},
+            {source: '4', target: '5', value: 6},
+            {source: '4', target: '6', value: 6},
+            {source: '5', target: '6', value: 1},
+            {source: '6', target: '7', value: 1},
+            {source: '6', target: '8', value: 8},
             {source: '6', target: '9', value: 3},
             {source: '7', target: '8', value: 4}
         ]
@@ -70,6 +76,20 @@ export class App extends Component
         this.svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
     }
 
+    private getLinks(links:Link[]):any[]
+    {
+        const maxValue = max(this.data.links, d => d.value);
+
+        return this.data.links.map(d => {
+            let newD = Object.create(d)
+            let gap = this.hexSize / 2;
+            newD.value = d.value < (maxValue *.5) ? 0 : d.value;
+            newD.distance = gap + ((maxValue * gap) - d.value * gap);
+            if(newD.distance > this._width / 3) newD.distance = this._width / 3
+            return newD;
+        });
+    }
+
     private onInit()
     {
         this._container = document.getElementById('root');
@@ -80,33 +100,41 @@ export class App extends Component
             
             window.addEventListener('resize', () => this.onResize())
             this.onResize();   
+
             const maxValue = max(this.data.links, d => d.value)
-            const links = this.data.links.map(d => {
-                let newD = Object.create(d)
-                let gap = this.hexSize;
-                newD.distance = gap + ((maxValue * gap) - (d.value) * gap);
-                newD.value = d.value < (maxValue *.4) ? 0 : d.value;
-                return newD;
-            });
+            const links = this.getLinks(this.data.links);
             const nodes = this.data.nodes.map(d => Object.create(d));
 
-            const simulation = forceSimulation(nodes)
+            this.sim = forceSimulation(nodes)
                 .force("link", forceLink(links).distance(d => d.distance))
-                .force("charge", forceManyBody().strength(-75))
+                .force("charge", forceManyBody().strength(-50))
                 .force("center", forceCenter(this._width / 2, this._height / 2))
                 .force('collision', forceCollide().radius(this.hexSize))
 
             const svg = select(this.svg);
 
-            const link = svg.append("g")
+            this.linkSelector = svg.append("g")
                 .attr("stroke", "#999")
-                //.attr("stroke-opacity", 1)
                 .selectAll("line")
                 .data(links)
                 .join("line")
+                
                 .attr("stroke-width", d => {
-                    let w = d.value / 2;
+                    let w = d.value * 0.75;
                     return w > 10 ? 10 : w;
+                }) ;
+
+            const linkDoubler = svg.append("g")
+                .attr("stroke", "#999")
+                .selectAll("line")
+                .data(links)
+                .join("line")
+                .attr('class', 'doubler')
+                .attr("stroke-width", d => {
+                    if(d.value < maxValue * 0.6) return 0;
+                    let w = d.value / 3;
+                    
+                    return w > 5 ? 5 : w;
                 }) ;
 
             const nodeOutline = svg.append("g")
@@ -114,6 +142,7 @@ export class App extends Component
                 .data(nodes)
                 .join("path")
                 .attr("class", 'hex outline')
+                
             
             const nodeInside = svg.append("g")
                 .selectAll("path")
@@ -126,6 +155,7 @@ export class App extends Component
                 .data(nodes)
                 .join("path")
                 .attr("class", 'hex center')
+                .call(() => this.drag(this.sim));
             
             const nodeLabel = svg.append("g")
                 .selectAll("text")
@@ -135,29 +165,69 @@ export class App extends Component
                 .attr("class", 'label')
                 .attr("alignment-baseline", 'middle')
 
-            // node.append("title")
-            //     .text(d => d.id);
-
-            simulation.on("tick", () => {
-                link
-                    .attr("x1", d => d.source.x)
-                    .attr("y1", d => d.source.y)
-                    .attr("x2", d => d.target.x)
-                    .attr("y2", d => d.target.y);
-
-                nodeOutline.attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize))
-                nodeInside.attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize * .9))
-                nodeCenter.attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize * .85))
+            this.sim.on("tick", () => 
+            {      
+                nodeOutline.style('opacity', this.opacity).attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize))
+                nodeInside.style('opacity', this.opacity).attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize * .9))
+                nodeCenter.style('opacity', this.opacity).attr("d", d => this.getPolygonPath(d.x, d.y, this.hexSize * .85))
                 
+                this.linkSelector
+                    .style('opacity', this.opacity)
+                    .attr("x1", (d:any) => this.getXY(d.source.x, d.source.y).x)
+                    .attr("y1", (d:any) => this.getXY(d.source.x, d.source.y).y)
+                    .attr("x2", (d:any) => this.getXY(d.target.x, d.target.y).x)
+                    .attr("y2", (d:any) => this.getXY(d.target.x, d.target.y).y);
+                
+                linkDoubler
+                    .style('opacity', this.opacity)
+                    .attr("x1", d => this.getXY(d.source.x, d.source.y).x)
+                    .attr("y1", d => this.getXY(d.source.x, d.source.y).y)
+                    .attr("x2", d => this.getXY(d.target.x, d.target.y).x)
+                    .attr("y2", d => this.getXY(d.target.x, d.target.y).y);
+
                 nodeLabel
-                    .attr("x", d => d.x)
-                    .attr("y", d => d.y)
+                    .style('opacity', this.opacity)
+                    .attr("x", d => this.getXY(d.x, d.y).x)
+                    .attr("y", d => this.getXY(d.x, d.y).y)
+                    .style('font-size', `${this.hexSize / 2}px`)
             });
 
-            //invalidation.then(() => simulation.stop());
-
-            //return svg.node();
+            setTimeout(() => 
+            {
+                for (var i = 1000; i > 0; --i) this.sim.tick();
+                this.opacity = 1;
+                
+            }, 10);
         }
+    }
+
+    private drag = (simulation:any) => 
+    {
+  
+        function dragstarted(d:any) 
+        {
+          if (!event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+        
+        function dragged(d:any) 
+        {
+          d.fx = event.x;
+          d.fy = event.y;
+        }
+        
+        function dragended(d:any) 
+        {
+          if (!event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
+        
+        return drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended);
     }
 
     private getLabel(node:Node):string
@@ -167,14 +237,28 @@ export class App extends Component
         return node.name.slice(0, 2);
     }
 
+    private getXY(x:number, y:number):{x:number, y:number}
+    {
+        let gridX = Math.round(this.hexSize * 1.7);
+        let gridY = Math.round(this.hexSize * 2);
+        x = Math.round(x / gridX) * gridX;
+        y = Math.round(y / gridY) * gridY;
+
+        if((x % (gridX * 2)) == 0) y += gridY / 2;
+
+        return {x: x, y: y};
+    }
+
     private getPolygonPath(x:number, y:number, radius:number, sides:number = 6, startAngle:number = 0) 
     {
         let d = ''
 
+        let xy = this.getXY(x, y);       
+        
         range(sides).map(side => 
         {
             const angle = startAngle + 2 * Math.PI * side / sides
-            return [x + (radius * Math.cos(angle)), y + (radius * Math.sin(angle))]
+            return [xy.x + (radius * Math.cos(angle)), xy.y + (radius * Math.sin(angle))]
         })
         .forEach(pt => 
         {
@@ -191,10 +275,24 @@ export class App extends Component
             this._width = this._container.offsetWidth;
             this._height = this._container.offsetHeight;
 
-            this.hexSize = this.width  * .06;
+            let sizeBy = this.width > this.height ? this.height : this.width;
+            // this.hexSize = sizeBy  * .06;
+            // if(this.hexSize > 60) this.hexSize = 60;
 
             this.svg.setAttribute('width', String(this._width));
             this.svg.setAttribute('height', String(this._height));
+
+            if(this.sim)
+            {
+                const links = this.getLinks(this.data.links);
+
+                this.linkSelector.data(links)
+
+                this.sim.force("link", forceLink(links).distance(d => d.distance))
+                this.sim.force("center", forceCenter(this._width / 2, this._height / 2));
+                this.sim.force("charge", forceManyBody().strength(-this.hexSize))
+                this.sim.restart();
+            }
         }
     }
     
